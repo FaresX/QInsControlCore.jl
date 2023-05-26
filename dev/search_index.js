dbe826a55a7861c27268e295bb5fe5b9ea3414f4
Documenter.jl <documenter@juliadocs.github.io>
2023-05-26 02:29:49 +0000

    build based on fa298d7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QInsControlCore","category":"page"},{"location":"#QInsControlCore","page":"Home","title":"QInsControlCore","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QInsControlCore.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QInsControlCore]","category":"page"}]
+}
