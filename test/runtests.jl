7374f8f5f4ecb133033c469ca3c2fc33e0aac968
FaresX <fyzxst@sina.com>
2023-05-26 16:24:59 +0800

    modified:   test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,49 @@ using QInsControlCore
 using Test
 
 @testset "QInsControlCore.jl" begin
-    # Write your tests here.
+    @test begin
+        cpu = Processor()
+        ct = Controller("VirtualInstr", "VirtualAddress")
+        login!(cpu, ct)
+        start!(cpu)
+        str = ct(write, cpu, "*IDN?", Val(:write))
+        logout!(cpu, ct)
+        stop!(cpu)
+        str == "done"
+    end
+    @test begin
+        cpu = Processor()
+        ct = Controller("VirtualInstr", "VirtualAddress")
+        login!(cpu, ct)
+        start!(cpu)
+        str = ct(read, cpu, Val(:read))
+        logout!(cpu, ct)
+        stop!(cpu)
+        str == "read"
+    end
+    @test begin
+        cpu = Processor()
+        ct = Controller("VirtualInstr", "VirtualAddress")
+        login!(cpu, ct)
+        start!(cpu)
+        str = ct(query, cpu, "*IDN?", Val(:query))
+        logout!(cpu, ct)
+        stop!(cpu)
+        str == "query"
+    end
+    @test begin
+        cpu = Processor()
+        start!(cpu)
+        islow = !cpu.fast[]
+        stop!(cpu)
+        islow
+    end
+    @test begin
+        cpu = Processor()
+        start!(cpu)
+        fast!(cpu)
+        isfast = cpu.fast[]
+        stop!(cpu)
+        isfast
+    end
 end
