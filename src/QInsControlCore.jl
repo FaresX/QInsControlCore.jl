0175a07db7f207a866a44947778f37fcf308c033
FaresX <39757092+FaresX@users.noreply.github.com>
2024-05-25 11:21:43 +0800

    update QInsControlCore

--- src/QInsControlCore.jl
+++ src/QInsControlCore.jl
@@ -1,12 +1,27 @@
 module QInsControlCore
-    using Instruments
-    using Sockets
-    using UUIDs
+using BinDeps
+using Instruments
+using Instruments: viSetAttribute
+using LibSerialPort
+using Sockets
+using UUIDs
 
-    export Controller, Processor
-    export login!, logout!, start!, stop!, reconnect!, find_resources, slow!, fast!
-    export instrument, connect!, disconnect!, write, read, query
 
-    include("Instruments.jl")
-    include("DataStream.jl")
+export Controller, Processor
+export login!, logout!, start!, stop!, reconnect!, find_resources, slow!, fast!
+export instrument, connect!, disconnect!, write, read, query
+
+function timedwhile(f::Function, timeout::Real)
+    t = time()
+    while time() - t < timeout
+        f() && return true
+        yield()
+    end
+    return false
+end
+
+include("VISA.jl")
+include("constants.jl")
+include("Instruments.jl")
+include("DataStream.jl")
 end # module QInsControlCore
